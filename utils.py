7202a30123b0460e7f5ccb28ac0a694da7d67c02
Majid Benam <benam.mjd@gmail.com>
2022-09-15 15:15:54 +0200

    functional code that saves all examples and defs and phon and pos. NICE.

--- utils.py
+++ utils.py
@@ -4,7 +4,10 @@ import requests
 import re
 import bs4
 import json
+import time
 
+
+from oxf_vars import *
 
 source_url = 'https://www.oxfordlearnersdictionaries.com/definition/english/'
 
@@ -14,11 +17,11 @@ headers = {
 }
 
 # 
-my_list = ["cushion"]
-my_words_list_of_dics = []
+#my_list = ["cushion", "maroon", "wanderer"]
 #source = requests.get(source_url, headers=headers).text
 # the def_counter problem is solved. The other problem is with so many try and except blocks
 # I will rewrite everything with simple if and if else and it will be good.
+
 
 def put_one_word_in(word, main_defs, words_dic):
     entry_dic = {}
@@ -41,7 +44,7 @@ def test_func(my_list):
         def_counter = 1
         main_defs_for_all_in_one_word = []
         for i in [1,2,3,4]:
-            print("Going for word: ", word, str(i))
+            print(word, str(i),  end=",")
             my_word_url = source_url + word + "_" + str(i)
             source = requests.get(my_word_url, headers=headers)
             soup = BeautifulSoup(source.content.decode('utf-8'), 'lxml')
@@ -49,7 +52,7 @@ def test_func(my_list):
             if main_defs_for_one:
                 main_defs_for_all_in_one_word.append(main_defs_for_one)
             else:
-                print("Itseems there were no definitions for: ", my_word_url)
+                #print("Itseems there were no definitions for: ", my_word_url)
                 continue
                 #main_defs_for_all_in_one_word.append("NO_DEFINITION")
         put_one_word_in(word, main_defs_for_all_in_one_word, words_dic)
@@ -58,47 +61,112 @@ def test_func(my_list):
 def test_func_smaller_soup(my_list):
     words_dic = {}
     for word in my_list:
-        def_counter = 1
-        main_defs_for_all_in_one_word = []
-        for i in [1,2,3,4]:
-            print("Going for word: ", word, str(i))
+        time.sleep(2)
+        inner_dic = {}
+        for i in [1,2,3,4,5,6]:
+            print(word, str(i), end=", ")
             my_word_url = source_url + word + "_" + str(i)
-            source = requests.get(my_word_url, headers=headers)
+            source = requests.get(my_word_url, headers=headers, verify=False)
+            # the big soup (not so useful)
             soup = BeautifulSoup(source.content.decode('utf-8'), 'lxml')
-            main_defs_for_one = soup.find_all('span', class_ = 'def')
-            if main_defs_for_one:
-                main_defs_for_all_in_one_word.append(main_defs_for_one)
-            else:
-                print("Itseems there were no definitions for: ", my_word_url)
-                continue
-                #main_defs_for_all_in_one_word.append("NO_DEFINITION")
-        put_one_word_in(word, main_defs_for_all_in_one_word, words_dic)
-    return words_dic
-
-def sng_situation_clearer(my_list):
-    words_dic = {}
-    for word in my_list:
-        main_defs_for_all_in_one_word = []
-        for i in [1,2,3]:
-            print("Going for word: ", word, str(i))
-            my_word_url = source_url + word + "_" + str(i)
-            source = requests.get(my_word_url, headers=headers)
-            soup = BeautifulSoup(source.content.decode('utf-8'), 'lxml')
+            # my smaller soups which will have all we need
             my_sngs = soup.find_all('li', {'id':re.compile(word + r'_sng_\d{1,2}')})
             if my_sngs:
                 #my_sngs_list.append(my_sngs)
-                for small_soup in my_sngs:
+                sng_dic = {}
+                # go loop through all the smaller soups
+                for small_soup_index, small_soup in enumerate(my_sngs):
+                    # REMEMBER: we have an i (the numbber on top)
+                    # and a small_soup_index which is the inner one.
+                    # get the definition
+
+                    # details_dic gets populated gradually in this loop
+                    details_dic = {}
+
                     my_def_inside = small_soup.find_all('span', class_ = 'def')
                     if my_def_inside:
-                        main_defs_for_all_in_one_word.append(my_def_inside)
+                        for def_index, my_def in enumerate(my_def_inside):
+                            def_key = "DEF_" + str(def_index)
+                            details_dic[def_key] = my_def.text
                     else:
-                        print("Itseems there were no definitions for: ", my_word_url)
-                        continue
-                    print(my_def_inside)
+                        def_key = "DEF_" + str(small_soup_index)
+                        details_dic[def_key] = "NO_DEFINITIONS"
+                        #print("It seems there were no definitions for: ", my_word_url)
+
+                    # get the examples
+                    main_examples = small_soup.find_all('span', class_ = 'x')
+                    if main_examples:
+                        for example_index, my_example in enumerate(main_examples):
+                            example_key = "EX_" + str(example_index)
+                            details_dic[example_key] = my_example.text
+                    else:
+                        example_key = "EX_" + str(small_soup_index)
+                        details_dic[example_key] = "NO_EXAMPLES"
+                        #print("It seems there were no EXAMPLES for: ", my_word_url)
+
+                    # get the phonetics
+                    my_american_pros = soup.find('div', class_="phons_n_am")
+                    my_american_pro = my_american_pros.find_all('span', class_ = 'phon')
+                    if my_american_pro:
+                        details_dic["PHON"] = my_american_pro[0].text
+                    else:
+                        details_dic["PHON"] = "NO_PHONS"
+                        #print("It seems there are no Phonetics for: ", my_word_url)
+
+                    # get the part of speech (from the main soup)
+                    my_pos = soup.find('span', class_ = 'pos')
+                    if my_pos:
+                        details_dic["POS"] = my_pos.text
+                    else:
+                        details_dic["POS"] = "NO_POS"
+                        #print("It seems there are no POS for: ", my_word_url)
+
+                    sng_dic[small_soup_index] = details_dic
+                    #print(my_def_inside)
+                inner_dic[i] = sng_dic
             else:
-                continue
-        put_one_word_in(word, main_defs_for_all_in_one_word, words_dic)
+                #print("NO_SNG: ", i)
+                break
+        words_dic[word] = inner_dic
+    with open("my_json_file_of_oxford_test_dic.json", "w") as outfile:
+       json.dump(words_dic, outfile)
     return words_dic
+
+
+#             main_defs_for_one = soup.find_all('span', class_ = 'def')
+#             if main_defs_for_one:
+#                 main_defs_for_all_in_one_word.append(main_defs_for_one)
+#             else:
+#                 print("Itseems there were no definitions for: ", my_word_url)
+#                 continue
+#                 #main_defs_for_all_in_one_word.append("NO_DEFINITION")
+#         put_one_word_in(word, main_defs_for_all_in_one_word, words_dic)
+#     return words_dic
+
+# def sng_situation_clearer(my_list):
+#     words_dic = {}
+#     for word in my_list:
+#         main_defs_for_all_in_one_word = []
+#         for i in [1,2,3]:
+#             print("Going for word: ", word, str(i))
+#             my_word_url = source_url + word + "_" + str(i)
+#             source = requests.get(my_word_url, headers=headers)
+#             soup = BeautifulSoup(source.content.decode('utf-8'), 'lxml')
+#             my_sngs = soup.find_all('li', {'id':re.compile(word + r'_sng_\d{1,2}')})
+#             if my_sngs:
+#                 #my_sngs_list.append(my_sngs)
+#                 for small_soup in my_sngs:
+#                     my_def_inside = small_soup.find_all('span', class_ = 'def')
+#                     if my_def_inside:
+#                         main_defs_for_all_in_one_word.append(my_def_inside)
+#                     else:
+#                         print("Itseems there were no definitions for: ", my_word_url)
+#                         continue
+#                     print(my_def_inside)
+#             else:
+#                 continue
+#         put_one_word_in(word, main_defs_for_all_in_one_word, words_dic)
+#     return words_dic
 
 
 
